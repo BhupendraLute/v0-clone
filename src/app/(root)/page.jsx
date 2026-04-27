e2f64aa1ba0cbe02a1923991775da1307a5fcb45
Bhupendra Lute <bhupendralute1234@gmail.com.com>
2026-04-27 17:37:04 +0530

    feat: add project form component and templates for project creation

--- src/app/(root)/page.jsx
+++ src/app/(root)/page.jsx
@@ -1,13 +1,35 @@
-import { Button } from "@/components/ui/button";
-import { UserButton } from "@clerk/nextjs";
+import ProjectForm from "@/modules/home/components/project-form";
 import Image from "next/image";
+import React from "react";
 
-export default function Home() {
+const page = () => {
 	return (
-		<div className="flex flex-col items-center justify-center h-screen">
-      <h1>hello</h1>
-      <Button >Button</Button>
-      <UserButton />
-    </div>
+		<div className="flex items-center justify-center w-full px-4 py-8">
+			<div className="max-w-5xl w-full">
+				<section className="space-y-8 flex flex-col items-center">
+					<div className="flex flex-col items-center">
+						<Image
+							src={"/logo.svg"}
+							width={100}
+							height={100}
+							alt="logo"
+							className="hidden md:block invert dark:invert-0"
+						/>
+					</div>
+					<h1 className="text-2xl md:text-5xl font-bold text-center">
+						Build Something with 💖
+					</h1>
+					<p className="text-lg md:text-xl text-muted-foreground text-center">
+						Create apps and websites by chatting with AI
+					</p>
+
+					<div className="max-w-3xl w-full">
+						<ProjectForm />
+					</div>
+				</section>
+			</div>
+		</div>
 	);
-}
+};
+
+export default page;
